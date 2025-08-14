ec96feed818e039fec8be1cef69cd0c690c6e7dd
kunal <kunalkumar9457.kk@gmail.com>
2025-08-15 00:47:13 +0530

    y3

--- services/stock_service.py
+++ services/stock_service.py
@@ -45,7 +45,8 @@ if _YF_SESSION is None:
 
 def _sleep_before_yf_call():
     if IS_RENDER:
-        time.sleep(random.uniform(3.0, 8.0))
+        # Fixed 3s delay for initial testing on cloud to avoid bursts
+        time.sleep(3.0)
     else:
         time.sleep(random.uniform(0.5, 2.0))
 
